5657c313f805be9b5136efa55fc6ac5192446cff
Rahul Raj <rahul.raj@keysight.com>
2019-01-21 19:00:05 +0530

    Added code for delete button in list-task component

--- src/app/list-task/card/card.component.ts
+++ src/app/list-task/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 @Component({
   selector: 'app-card',
@@ -8,8 +8,13 @@ import { Task } from '../../Task';
 export class CardComponent implements OnInit {
 
   @Input() public task: Task;
-  // date1: Date = this.task.endDate;
+  @Output() public deletedTaskEvent = new EventEmitter();
   constructor() { }
+
+  onDelete()
+  {
+    this.deletedTaskEvent.emit(this.task);
+  }
 
   ngOnInit() {
   }
